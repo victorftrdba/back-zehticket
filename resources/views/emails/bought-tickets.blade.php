98cfc9a6b8eb9f1eb2507aeb1d55d7736aa36955
Victor Nogueira <victornogu80@gmail.com>
2022-06-29 15:23:17 -0300

    correção qrcode

--- resources/views/emails/bought-tickets.blade.php
+++ resources/views/emails/bought-tickets.blade.php
@@ -14,9 +14,9 @@
             <div style="margin-bottom:15px;">
                 <div>QRCode:
                 <?php
-                    $qrCodeAsPng = QrCode::format('png')->size(500)->generate($code->code);
+                    $qrCodeAsPng = QrCode::format('png')->size(200)->encoding('UTF-8')->generate($code->code);
                 ?>
-                <img src="{!! $message->embedData($qrCodeAsPng, 'QRCode.png', 'image/png') !!}">
+                <img src="{!!$message->embedData($qrCodeAsPng, 'QrCode.png', 'image/png')!!}">
                 </div>
                 <div>Ingresso: <b>{{ $code->ticket->description }} | {{ $code->code }}</b></div>
                 <div>Evento: <b>{{ $code->event->title }}</b></div>
