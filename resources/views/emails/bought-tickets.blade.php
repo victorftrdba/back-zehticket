7d61506584a204a5c520fe44c3e6a56ebd6c8657
Victor Nogueira <victornogu80@gmail.com>
2022-06-29 15:41:38 -0300

    Atualizando layout de disparo de e-mail

--- resources/views/emails/bought-tickets.blade.php
+++ resources/views/emails/bought-tickets.blade.php
@@ -9,19 +9,25 @@
 </head>
 
 <body>
-    <div style="display:flex;justify-content:center;align-items:center;flex-direction:column;">
-        @foreach ($codes as $code)
-            <div style="margin-bottom:15px;">
-                <div>QRCode:</div>
-                <?php
-                    $qrCodeAsPng = (string) QrCode::format('png')->margin(0)->size(200)->generate($code->code);
-                ?>
-                <img src="{!! $message->embedData($qrCodeAsPng, 'QrCode.png', 'image/png')!!}">
-                <div>Ingresso: <b>{{ $code->ticket->description }} | {{ $code->code }}</b></div>
-                <div>Evento: <b>{{ $code->event->title }}</b></div>
-                <div>Data do Evento: <b>{{ \Carbon\Carbon::parse($code->event->date)->format('d/m/y') }}</b></div>
-            </div>
-        @endforeach
+    <div style="font-family: Roboto">
+        <div style="text-align:center">
+            <h2>Olá! Agradecemos a aquisição dos ingressos. <br/> Aproveite ao máximo seu evento!</h2>
+            @foreach ($codes as $code)
+                <div style="margin-bottom:15px;">
+                    <div><b>QRCode</b></div>
+                    <?php
+                        $qrCodeAsPng = (string) QrCode::format('png')->margin(0)->size(200)->generate($code->code);
+                    ?>
+                    <img src="{!! $message->embedData($qrCodeAsPng, 'QrCode.png', 'image/png')!!}" style="margin-bottom:5px;">
+                    <div><b>INGRESSO</b></div>
+                    <div style="margin-bottom:5px;"><b>{{ $code->ticket->description }} | {{ $code->code }}</b></div>
+                    <div><b>EVENTO</b></div>
+                    <div style="margin-bottom:5px;"><b>{{ $code->event->title }}</b></div>
+                    <div><b>DATA DO EVENTO</b></div>
+                    <div><b>{{ \Carbon\Carbon::parse($code->event->date)->format('d/m/y') }}</b></div>
+                </div>
+            @endforeach
+        </div>
     </div>
 </body>
 
