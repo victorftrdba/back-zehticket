60a4b55dd7ac66286b6cb4ac6cf54f5f65876ea5
Victor Nogueira <victornogu80@gmail.com>
2022-06-29 11:47:49 -0300

    reverting configs

--- config/mail.php
+++ config/mail.php
@@ -36,11 +36,11 @@ return [
     'mailers' => [
         'smtp' => [
             'transport' => 'smtp',
-            'host' => env('MAIL_HOST', 'smtp.mailtrap.io'),
-            'port' => env('MAIL_PORT', 2525),
+            'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+            'port' => env('MAIL_PORT', 587),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
-            'username' => env('MAIL_USERNAME', '28acaed5902a72'),
-            'password' => env('MAIL_PASSWORD', 'afb31c7e63054a'),
+            'username' => env('MAIL_USERNAME'),
+            'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
         ],
 
@@ -112,14 +112,6 @@ return [
         'paths' => [
             resource_path('views/vendor/mail'),
         ],
-    ],
-
-    'stream' => [
-      'ssl' => [
-          'allow_self_signed' => true,
-          'verify_peer' => false,
-          'verify_peer_name' => false,
-      ],
     ],
 
 ];
